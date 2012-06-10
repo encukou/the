b756ce2ca00ab7e1dcf71922ea0478a96479869b
Petr Viktorin <encukou@gmail.com>
2012-06-10 02:41:27 +0200

    Demostrate that emu.frameadvance() now works

--- scripts/importteams.lua
+++ scripts/importteams.lua
@@ -11,8 +11,16 @@ copy_file = function(name, offset)
     file:close()
 end
 
+print("Frame count", emu.framecount())
+
 memory.writebyte(0x0225d75c, 3)  -- player party size
 memory.writebyte(0x0225dcbc, 4)  -- opponent party size
 
 copy_file('player.pkms', 0x0225d760)
 copy_file('opponent.pkms', 0x0225dcc0)
+
+print("Frame count", emu.framecount())
+for i=1, 100, 1 do
+    emu.frameadvance()
+    print("Frame count", emu.framecount())
+end
